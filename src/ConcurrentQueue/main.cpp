2f9b7ea2cfaabc6708bae695432399f81fe5053d
shines77 <gz_shines@msn.com>
2016-10-28 15:36:18 +0800

    Changes: QtSignal class have been removed and create a new repository "QtSignal" on my github.
    Signed-off-by: shines77 <gz_shines@msn.com>

--- src/ConcurrentQueue/main.cpp
+++ src/ConcurrentQueue/main.cpp
@@ -24,9 +24,6 @@
 #include "this_thread.h"
 #include "stop_watch.h"
 
-#include "QTSignal.h"
-#include "Functional.h"
-
 #if defined(NDEBUG)
 static const size_t kMaxMessageCount = (800 * 10000);
 #else
@@ -331,7 +328,7 @@ void run_queue_test(unsigned message_count, unsigned producers, unsigned consume
     run_queue_test_impl<FixedLockedRingQueueWrapper<Message, std::mutex, index_type, Capacity>, Message>(message_count, producers, consumers, Capacity);
 #endif
 
-    ///*
+#if 1
     if (producers == 1 && consumers == 1) {
         printf("-------------------------------------------------------------------------\n");
         run_queue_test_impl<DisruptorRingQueueWrapper<Message, sindex_type, Capacity, 1, 1, 2>, Message>(message_count, producers, consumers, Capacity);
@@ -352,7 +349,7 @@ void run_queue_test(unsigned message_count, unsigned producers, unsigned consume
         printf("-------------------------------------------------------------------------\n");
         run_queue_test_impl<DisruptorRingQueueWrapper<Message, sindex_type, Capacity, 8, 8, 16>, Message>(message_count, producers, consumers, Capacity);
     }
-    //*/
+#endif
 
     //printf("-------------------------------------------------------------------------\n");
 }
@@ -417,110 +414,12 @@ void run_unittest()
 #endif
 }
 
-#include "QtSignal.h"
-
-enum signal_slot {
-    OnValueChange,
-    OnScrollChange
-};
-
-class FooA {
-public:
-    void onValueChange(int index) {
-        std::cout << "FooA::onValueChange(int index): index = " << index << std::endl;
-    }
-
-    void onValueChange2(int index, int x, int y) {
-        std::cout << "FooA::onValueChange2(int index, int x, int y): index = " << index << std::endl;
-    }
-};
-
-class FooB {
-public:
-    void onValueChange(int index) {
-        std::cout << "FooB::onValueChange(int index): index = " << index << std::endl;
-    }
-
-    void onValueChange2(int index, int x, int y) {
-        std::cout << "FooB::onValueChange2(int index, int x, int y): index = " << index << std::endl;
-    }
-};
-
-/*
-template <typename R, typename Arg>
-bool function_equal(std::function<R(Arg)> const & func1, std::function<R(Arg)> const & func2) _NOEXCEPT {
-    R (* const * ptr1)(Arg) = func1.target<R(*)(Arg)>();
-    R (* const * ptr2)(Arg) = func2.target<R(*)(Arg)>();
-    return ((ptr1 != nullptr) && (ptr2 != nullptr) && (ptr1 == ptr2));
-}
-//*/
-
-/*
-template <typename R, typename T, typename ...Args>
-bool function_equal(std::function<R(Args...)> const & func1, std::function<R(Args...)> const & func2) _NOEXCEPT {
-    R (* const * ptr1)(T *, Args...) = func1.template target<R(*)(T *, Args...)>();
-    R (* const * ptr2)(T *, Args...) = func2.template target<R(*)(T *, Args...)>();
-    return ((ptr1 != nullptr) && (ptr2 != nullptr) && (ptr1 == ptr2));
-}
-//*/
-
-template <typename R, typename ...Args>
-inline bool operator == (std::function<R(Args...)> const & func1, std::function<R(Args...)> const & func2) _NOEXCEPT {
-    R (* const * ptr1)(Args...) = func1.template target<R(*)(Args...)>();
-    R (* const * ptr2)(Args...) = func2.template target<R(*)(Args...)>();
-    return ((ptr1 != nullptr) && (ptr2 != nullptr) && (ptr1 == ptr2));
-}
-
-template <typename R, typename ...Args>
-inline bool operator == (std::function<R(Args...)> && func1, std::function<R(Args...)> && func2) _NOEXCEPT {
-    return true;
-}
-
-void test_signal()
-{
-    using namespace std::placeholders;
-    typedef jimi::signal<> signal_0;
-    signal_0 & signal_0_inst = signal_0::get();
-
-    signal_0_inst.connect(signal_slot::OnValueChange, []() { std::cout << "lambda::onValueChange()." << std::endl; });
-    signal_0_inst.emit(signal_slot::OnValueChange);
-    signal_0_inst.disconnect(signal_slot::OnValueChange);
-
-    typedef jimi::signal<int> signal_int;
-    signal_int & signal_int_inst = signal_int::get();
-
-    FooA a; FooB b;
-    std::function<void(int)> memfunc_a1 = std::bind(&FooA::onValueChange, &a, _1);
-    std::function<void(int)> memfunc_a2 = std::bind(&FooA::onValueChange, &a, _1);
-    std::function<void(int)> memfunc_b = std::bind(&FooB::onValueChange, &b, _1);
-    printf("memfunc_a1.target_type().name() = %s\n\n", memfunc_a1.target_type().name());
-    printf("jimi::getAddressOf(memfunc_a1) = 0x%p\n", (void *)jimi::getAddressOf(memfunc_a1));
-    printf("jimi::getAddressOf(memfunc_a2) = 0x%p\n", (void *)jimi::getAddressOf(memfunc_a2));
-    printf("\n");
-    //std::function<void(FooA *, int)> memfunc_a(FooA::onValueChange, &a, _1);
-    printf("(memfunc_a1 == memfunc_a2) ? %d\n", (int)(memfunc_a1 == memfunc_a2));
-    printf("(memfunc_a1 == memfunc_a2) ? %d\n", (int)jimi::template function_equal<FooA>(memfunc_a1, memfunc_a2));
-    signal_int_inst.connect(signal_slot::OnValueChange, memfunc_a1);
-    signal_int_inst.connect(signal_slot::OnValueChange, memfunc_b);
-    signal_int_inst.emit(signal_slot::OnValueChange, 100);
-    signal_int_inst.disconnect(signal_slot::OnValueChange);
-
-    std::function<void(int, int, int)> memfunc_a1_ = std::bind(&FooA::onValueChange2, &a, _1, _2, _3);
-    std::function<void(int, int, int)> memfunc_a2_ = std::bind(&FooA::onValueChange2, &a, _1, _2, _3);
-
-    printf("memfunc_a1_.target_type().name() = %s\n\n", memfunc_a1_.target_type().name());
-    printf("(memfunc_a1_ == memfunc_a2_) ? %d\n", (int)jimi::template function_equal<FooA>(memfunc_a1_, memfunc_a2_));
-}
-
 int main(int argn, char * argv[])
 {
 #if !defined(NDEBUG)
     run_unittest();
 #endif
 
-    test_signal();
-
-#if 0
     run_queue_test<4096>(kMaxMessageCount, 1, 1);
     run_queue_test<8192>(kMaxMessageCount, 1, 1);
     run_queue_test<16384>(kMaxMessageCount, 1, 1);
@@ -546,7 +445,6 @@ int main(int argn, char * argv[])
     run_queue_test<65536>(kMaxMessageCount, 4, 4);
 
     printf("-------------------------------------------------------------------------\n");
-#endif
 
 #if defined(_WIN32) && (defined(NDEBUG) || !defined(NDEBUG))
     system("pause");
